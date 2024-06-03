1b9416ead12b6ef5fdf09ef3366fc19976d9d457
LuzChuquimia <luzchuquimia1680@gmail.com>
2024-06-03 16:59:18 -0300

    Aprendimos react

---
+++ REACT/vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import { Contenido } from "./components/miPrimerComponente/MiPrimerComponente";
+function App() {
+  return (
+    <>
+      <Contenido name={"Gris"} lastName={"Nieve"} />;
+    </>
+  );
+}
+
+export default App;
---
+++ REACT/vite-project/src/components/miPrimerComponente/MiPrimerComponente.jsx
@@ -0,0 +1,8 @@
+export function Contenido({ name, lastName }) {
+  return (
+    <>
+      <img src="./public/imgs/cat.webp" />
+      Hola!, mi nombre es {name} {lastName}, mucho gusto:D
+    </>
+  );
+}
